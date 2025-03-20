71fb89ebffaf1f52dd5ea9fd0894275a9668e96f
TuNA <nguyentu2392@gmail.com>
2025-03-20 22:22:21 +0700

    get and add post using react query

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -4,17 +4,28 @@ import "./posts.scss";
 import { useQuery } from "@tanstack/react-query";
 const Posts = () => {
 
-  const { isLoading, error, data } = useQuery(['posts', ()=>{
-    makeRequest.get("/posts").then(res =>{
-      return res.data
-    })
-  }])
-  
-  return <div className="posts">
-    {data.map(post=>(
-      <Post post={post} key={post.id}/>
-    ))}
-  </div>;
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      const res = await makeRequest.get("/posts");
+      return res.data;
+    }
+  });
+
+  console.log(data);
+
+
+  return (
+    <div className="posts">
+      {error ? (
+        "Something went wrong!"
+      ) : isLoading ? (
+        "Loading..."
+      ) : (
+        data.map((post) => <Post post={post} key={post.id} />)
+      )}
+    </div>
+  );
 };
 
 export default Posts;
--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -2,12 +2,36 @@ import "./share.scss";
 import Image from "../../assets/img.png";
 import Map from "../../assets/map.png";
 import Friend from "../../assets/friend.png";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { makeRequest } from "../../axios";
+import axios from "axios";
 
 const Share = () => {
+  const [file, setFile] = useState(null);
+  const [desc, setDesc] = useState("");
 
-  const {currentUser} = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext)
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async (newPost) => {
+      const res = await makeRequest.post("/posts", newPost);
+      return res.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["post"])
+    },
+  })  
+
+  const handleClick = e => {
+    e.preventDefault();
+    mutation.mutate({desc})
+
+  }
+
   return (
     <div className="share">
       <div className="container">
@@ -16,12 +40,12 @@ const Share = () => {
             src={currentUser.profilePic}
             alt=""
           />
-          <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} />
+          <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} onChange={(e) => setDesc(e.target.value)} />
         </div>
         <hr />
         <div className="bottom">
           <div className="left">
-            <input type="file" id="file" style={{display:"none"}} />
+            <input type="file" id="file" style={{ display: "none" }} onChange={(e) => setFile(e.target.files[0])} />
             <label htmlFor="file">
               <div className="item">
                 <img src={Image} alt="" />
@@ -38,7 +62,7 @@ const Share = () => {
             </div>
           </div>
           <div className="right">
-            <button>Share</button>
+            <button onClick={handleClick}>Share</button>
           </div>
         </div>
       </div>
